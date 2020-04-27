6d80ec50a8af532da51284adfbc682f7ba9d3e17
Cyril Cohen <cohen@crans.org>
2020-04-27 17:54:16 +0200

    update

--- default.nix
+++ default.nix
@@ -3,45 +3,47 @@
   config ? (pkgs: {}),
   withEmacs ? false,
   print-env ? false,
-  package ? "mathcomp-fast",
-  src ? config.${package} or ./.,
+  package ? "mathcomp-fast"
 }:
 with builtins;
 let
-  pre-cfg-fun = if isAttrs config then (_: config) else config;
-  cfg-fun = version: pre-cfg-fun version // { ${package} = src; };
+  cfg-fun = if isFunction config then config else (pkgs: config);
   pkgs = import nixpkgs {
-    config.packageOverrides = pkgs: with pkgs.lib; {
-      coqPackages = with pkgs; {
+    config.packageOverrides = pkgs: with pkgs.lib;
+      let coqPackages = with pkgs; {
         "8.7" = coqPackages_8_7;
         "8.8" = coqPackages_8_8;
         "8.9" = coqPackages_8_9;
         "8.10" = coqPackages_8_10;
         "8.11" = coqPackages_8_11;
-        "default" = coqPackages;
+        "default" = coqPackages_8_10;
       }.${(cfg-fun pkgs).coq or "default"}.overrideScope'
         (self: super:
           let
-            cfg = cfg-fun (pkgs // {coqPackages = self;});
-            mathcomp-full-config = lib.recursiveUpdate super.mathcomp-extra-config {
-                for-package = {
-                  mathcomp-fast = version: {
-                    propagatedBuildInputs = with self; ([ mathcomp ] ++ mathcomp-extra-fast);
-                  };
-                  mathcomp-full = version: {
-                    propagatedBuildInputs = with self; ([ mathcomp ] ++ mathcomp-extra-all);
-                  };
-                };
+            all-pkgs = pkgs // { coqPackages = self; };
+            cfg = { ${package} = ./.; } // {
+              mathcomp-fast = {
+                src = ./.;
+                propagatedBuildInputs = with self; ([ mathcomp ] ++ mathcomp-extra-fast);
               };
+              mathcomp-full = {
+                src = ./.;
+                propagatedBuildInputs = with self; ([ mathcomp ] ++ mathcomp-extra-all);
+              };
+            } // (cfg-fun all-pkgs);
           in {
-            mathcomp-extra-config = lib.recursiveUpdate mathcomp-full-config {
-              for-package = mapAttrs
-                (name: value: version: super.mathcomp-extra-override name cfg.${name} (value version))
-                cfg;
-            };
+            # mathcomp-extra-config = lib.recursiveUpdate super.mathcomp-extra-config {
+            #   for-coq-and-mc.${self.coq.coq-version}.${self.mathcomp.version} =
+            #     removeAttrs cfg ["mathcomp" "coq"];
+            # };
             mathcomp = if cfg?mathcomp then self.mathcomp_ cfg.mathcomp else super.mathcomp;
-          });
-    };
+          } // mapAttrs
+            (package: version: coqPackages.mathcomp-extra package version)
+            (removeAttrs cfg ["mathcomp" "coq"])
+        ); in {
+          coqPackages = coqPackages.filterPackages coqPackages.coq coqPackages;
+          coq = coqPackages.coq;
+        };
   };
 
   mathcompnix = ./.;
@@ -69,7 +71,7 @@ let
   emacs = with pkgs; emacsWithPackages
     (epkgs: with epkgs.melpaStablePackages; [proof-general]);
 
-  pkg = with pkgs; coqPackages.${package} or (coqPackages.mathcomp-extra package src);
+  pkg = with pkgs; coqPackages.${package} or (coqPackages.current-mathcomp-extra package);
 in
 if pkgs.lib.trivial.inNixShell then pkg.overrideAttrs (old: {
   inherit shellHook mathcompnix;
