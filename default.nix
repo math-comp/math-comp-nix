077ec3c4875fc1c9a62344dc2813874746444b39
Cyril Cohen <cohen@crans.org>
2020-06-14 02:27:27 +0200

    arbitrary coq

--- default.nix
+++ default.nix
@@ -13,15 +13,38 @@ let
   pkg-src = if src == false then {}
             else { ${if package == "mathcomp.single" then "mathcomp" else package} = src; };
   pkgs = if isAttrs nixpkgs then nixpkgs else import nixpkgs {
-    overlays = [ (pkgs: super-pkgs: with pkgs.lib;
-      let coqPackages = with pkgs; {
+    overlays = [ (pkgs: super-pkgs: with pkgs.lib // pkgs.stdenv.lib; let
+      getVersion = arg:
+        if isFunction arg then (arg {}).version
+        else  if arg == null || arg == "" then "master"
+        else  if isDerivation arg then arg.drvAttrs.version or getVersion (arg.src or null)
+        else  if isAttrs arg then arg.drvAttrs.version  or getVersion (arg.src or null)
+        else  if isString arg then head (reverseList (split "/" arg))
+        else  if isPath arg   then (baseNameOf arg)
+        else "master";
+      getAttrs = generic: let version = getVersion generic; in
+        if isFunction generic then generic {}
+        else  if generic == null || generic == "" then getAttrs "master"
+        else  if isDerivation generic then generic.drvAttrs
+        else  if isAttrs generic then generic
+        else  if generic == "broken" then { meta.broken = true; }
+        else  if isString generic then
+          let splitted = filter isString (split "/" generic); in {
+             src = fetchTarball ("https://github.com/" +
+               (if length splitted == 1 then "coq/coq/archive/${version}.tar.gz"
+               else "${head splitted}/coq/archive/${concatStringsSep "/" (tail splitted)}.tar.gz"));
+              }
+        else  if isPath generic then { src = generic; }
+        else abort "${toString generic} not a legitimate generic version/override";
+      coqPackages = with pkgs; ({
         "8.7" = coqPackages_8_7;
         "8.8" = coqPackages_8_8;
         "8.9" = coqPackages_8_9;
         "8.10" = coqPackages_8_10;
         "8.11" = coqPackages_8_11;
         "default" = coqPackages_8_10;
-      }.${(cfg-fun pkgs).coq or "default"}.overrideScope'
+      }.${(cfg-fun pkgs).coq or "default"} or
+        (mkCoqPackages (super-pkgs.coq.overrideAttrs (o: getAttrs ((cfg-fun pkgs).coq or {}))))).overrideScope'
         (coqPackages: super-coqPackages:
           let
             all-pkgs = pkgs // { inherit coqPackages; };
