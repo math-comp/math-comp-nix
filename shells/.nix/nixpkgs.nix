ff3a7c63576fe45dc6ea8d8395c0a83afb08f32b
Cyril Cohen <cohen@crans.org>
2021-03-11 23:06:16 +0100

    testing new mathcomp-analysis

---
+++ shells/.nix/nixpkgs.nix
@@ -0,0 +1,4 @@
+fetchTarball {
+         url = https://github.com/CohenCyril/nixpkgs/archive/f16c416fc6feab978b95cfd0776d0813c7e4fe08.tar.gz;
+         sha256 = "152dfhzvn2kyslga568vswqsq9gj6hhbig62145vl4wzcrjzzczg";
+       }
