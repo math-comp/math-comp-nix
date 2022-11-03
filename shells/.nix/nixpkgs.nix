41c866b82aaa1f3aa2a1179bef4c43a7392f0a8b
Cyril Cohen <cohen@crans.org>
2022-11-03 15:19:16 +0100

    update toolbox

--- shells/.nix/nixpkgs.nix
+++
@@ -1,4 +0,0 @@
-fetchTarball {
-         url = https://github.com/CohenCyril/nixpkgs/archive/f16c416fc6feab978b95cfd0776d0813c7e4fe08.tar.gz;
-         sha256 = "152dfhzvn2kyslga568vswqsq9gj6hhbig62145vl4wzcrjzzczg";
-       }
