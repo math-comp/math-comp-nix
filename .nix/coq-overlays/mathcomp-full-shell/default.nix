aa72ddf21b977cc7a7546d1f36d6f2d734b2b00c
Cyril Cohen <cohen@crans.org>
2023-06-14 15:46:51 +0200

    test

--- .nix/coq-overlays/mathcomp-full-shell/default.nix
+++ .nix/coq-overlays/mathcomp-full-shell/default.nix
@@ -2,11 +2,12 @@
   mathcomp-real-closed, mathcomp-analysis, multinomials,
   mathcomp-abel, version ? null }@args:
 stdenv.mkDerivation {
-  name = "mathcomp-full-shell";
-  theargs = builtins.trace args args;
-  buildInputs =
-    with builtins; filter (x: !(x.meta.broken or false))
-      (attrValues (removeAttrs theargs ["stdenv" "version"]));
-  src = version;
-  meta = {inherit mathcomp;};
+  name = "mathcomp-full-shell2";
+  buildInputs = [ ];
+  # theargs = builtins.trace args args;
+  # buildInputs =
+  #   with builtins; filter (x: !(x.meta.broken or false))
+  #     (attrValues (removeAttrs theargs ["stdenv" "version"]));
+  # src = version;
+  # meta = {inherit mathcomp;};
 }
